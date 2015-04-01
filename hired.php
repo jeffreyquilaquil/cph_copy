5fb194ad2a4167d198f6e1d5528d12b3b2781512
divi <ludivina.marinas@tatepublishing.net>
2015-03-31 20:25:26 -0500

    hr

--- hired.php
+++ hired.php
@@ -128,7 +128,7 @@ if(isset($_POST) AND !empty($_POST)){
 				
 		if($_POST['office'] == 'cebu'){
 			$to = 'helpdesk.cebu@tatepublishing.net,diana.bartulin@tatepublishing.net,hr.cebu@tatepublishing.net,'.$supEmail.','.$reqEmail;
-			$from = 'hr.cebu@tatepublishing.net';			
+			$from = 'kent.ybanez@tatepublishing.net';			
 		}else{
 			$to = 'helpdesk.us@tatepublishing.net';
 			$from = 'vikki.williams@tatepublishing.net';
