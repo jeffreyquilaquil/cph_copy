d019c814d7cac839fa4eb5f0d9945a09bbbc0c74
Career PH <ludivina.marinas@tatepublishing.net>
2015-10-07 23:53:58 -0500

    hehe

--- staff/application/controllers/timecard.php
+++ staff/application/controllers/timecard.php
@@ -37,9 +37,12 @@ class Timecard extends MY_Controller {
 	}
 		
 	public function timetest(){
-		$this->timeM->publishLogs('2015-10-05');
-		$this->timeM->publishLogs('2015-10-06');
-		$this->timeM->publishLogs('2015-10-07');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-01');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-02');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-03');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-05');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-06');
+		$this->timeM->cntUpdateAttendanceRecord('2015-10-07');
 	}
 		
 	//runs everyday at 12am
