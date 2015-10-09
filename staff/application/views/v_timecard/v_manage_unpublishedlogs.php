cb5edaa384c8db9f01a4310b4a21d76ea231b311
Career PH <ludivina.marinas@tatepublishing.net>
2015-10-08 21:07:38 -0500

    meditation

--- staff/application/views/v_timecard/v_manage_unpublishedlogs.php
+++ staff/application/views/v_timecard/v_manage_unpublishedlogs.php
@@ -11,7 +11,7 @@
 		
 		foreach($pubByDate AS $d8=>$dbyd){
 			echo '<table class="tableInfo">';
-				echo '<tr class="trlabel"><td colspan=4>'.date('F d, Y', strtotime($d8)).' ('.count($dbyd).')</td></tr>';
+				echo '<tr class="trlabel"><td colspan=4>'.date('l, F d, Y', strtotime($d8)).' ('.count($dbyd).')</td></tr>';
 				echo '<tr class="trhead">
 						<td width="30%">Name</td>
 						<td width="30%">Schedule</td>
@@ -19,12 +19,24 @@
 						<td>Details</td>
 					</tr>';
 				foreach($dbyd AS $d){
-					echo '<tr>
-							<td><a href="'.$this->config->base_url().'timecard/'.$d->empID_fk.'/?d='.$d->logDate.'" target="_blank">'.$d->name.'</a></td>
-							<td>'.date('h:i a', strtotime($d->schedIn)).' - '.date('h:i a', strtotime($d->schedOut)).'</td>
-							<td>'.date('h:i a', strtotime($d->timeIn)).' - '.date('h:i a', strtotime($d->timeOut)).'</td>
-							<td><a href="'.$this->config->base_url().'timecard/'.$d->empID_fk.'/viewlogdetails/?d='.$d->logDate.'" class="iframe"><img src="'.$this->config->base_url().'css/images/view-icon.png"/></a></td>
-						</tr>';
+					$sched = '';
+					if($d->schedIn=='0000-00-00 00:00:00' && $d->schedOut=='0000-00-00 00:00:00') $sched = 'None';
+					else $sched = date('h:i a', strtotime($d->schedIn)).' - '.date('h:i a', strtotime($d->schedOut));
+					
+					$timeOut = '';
+					if($d->timeIn=='0000-00-00 00:00:00' && $d->timeOut=='0000-00-00 00:00:00') $timeOut = 'None';
+					else{
+						if($d->timeIn!='0000-00-00 00:00:00') $timeOut =  date('h:i a', strtotime($d->timeIn));
+						if($d->timeOut!='0000-00-00 00:00:00') $timeOut .=  ' - '.date('h:i a', strtotime($d->timeOut));
+						else $timeOut .= ' - <span class="errortext">NO TIME OUT</span>';
+					}					
+					
+					echo '<tr>';
+						echo '<td><a href="'.$this->config->base_url().'timecard/'.$d->empID_fk.'/?d='.$d->logDate.'" target="_blank">'.$d->name.'</a></td>';
+						echo '<td>'.$sched.'</td>';							
+						echo '<td>'.$timeOut.'</td>';
+						echo '<td><a href="'.$this->config->base_url().'timecard/'.$d->empID_fk.'/viewlogdetails/?d='.$d->logDate.'" class="iframe"><img src="'.$this->config->base_url().'css/images/view-icon.png"/></a></td>';							
+					echo '</tr>';
 				}
 			echo '</table>';
 		}
