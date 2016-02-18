e575bbcbba3db8b16b200bc57c564bb33f394f80
marjune <marjune.abellana@tatepublishing.net>
2016-02-18 16:12:13 -0600

    search employee for supervisor

--- staff/application/views/includes/header_searchemployee.php
+++ staff/application/views/includes/header_searchemployee.php
@@ -1,8 +1,22 @@
 <?php
-	if($this->access->accessFullHR==true){
-		$allStaffs = $this->dbmodel->getQueryResults('staffs', 'empID, fname, lname, active');
+if($this->access->accessFullHR==true OR $this->user->is_supervisor == 1){
+        //if is supervisor search only your team
+    if( $this->user->is_supervisor == 1 ){
+            $ids = '';
+            $condition = '';
+            $my_staff = $this->commonM->getStaffUnder( $this->user->empID, $this->user->level );
+            foreach( $my_staff as $m ){
+                $ids .= $m->empID.',';
+            }
+            if( $ids != '' ) {
+                $condition .= ' AND empID IN ('.rtrim($ids,',').')';
+            }
+            $allStaffs = $this->dbmodel->getQueryResults('staffs', 'empID, lname, fname, active', 'active=1'.$condition, '', 'lname');
+        } else {
+    		$allStaffs = $this->dbmodel->getQueryResults('staffs', 'empID, fname, lname, active');
+        }
 		
-		echo '<div class="floatright" style="width:300px;">';
+        echo '<div class="floatright" style="width:300px;">';
 			echo $this->textM->formfield('text', '', '', 'forminput', 'Search Employee', 'id="searchTimeEmp" style="border:1px solid #800000;"');
 			echo '<div id="divstaffs" style="position:absolute; width:300px; z-index:999;">';
 				echo '<table id="timetable" class="hidden" style="background-color:#ccc; width:100%; border:1px solid #800000;">';
