2624bb2b6c05d69fec41c2e0f52552e3524b784a
rene carpio <rene.carpio@tatepublishing.net>
2016-04-07 21:34:38 -0500

    hr message model

---
+++ staff/application/models/ask_hr.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ask_hr extends CI_Model {
+	
+    function __construct() {
+        // Call the Model constructor
+        parent::__construct();		
+		//$this->load->model('timecardmodel', 'timeM');
+    }
+
+   function askhr($data){
+   		date_default_timezone_set("Asia/Manila");
+   		$str = $this->db->insert_string('hr_cs_post', $data);
+   		return $this->db->insert_id();
+
+   }
+  // function get_new_max_ID(){
+   	// $sql=$this->db->query("SELECT MAX(id) as id FROM hr_cs_post");
+    //	return $sql->row_array();
+   //}
+  function insertQuery($table, $array){
+		date_default_timezone_set("Asia/Manila");
+		if(count($array) > 0){
+			$sql = "INSERT INTO $table (";
+			$cols = '';
+			$vals = '';
+			foreach($array AS $k => $v){
+				$cols .= '`'.$k.'`,';
+				if($v=='NOW()')
+					$vals .= $v.',';
+				else
+					$vals .= '"'.$v.'",';
+			}
+			$sql .= rtrim($cols,',').') VALUES ('.rtrim($vals,',').')';
+			
+			$this->db->query($sql);
+			return $this->db->insert_id();
+		}	
+	}
+}
+
+?>
