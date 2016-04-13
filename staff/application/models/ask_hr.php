43db236e2b7866e973415a46dd236cc9d97995ad
rene carpio <rene.carpio@tatepublishing.net>
2016-04-12 20:27:31 -0500

    new funtion for hr help desk

--- staff/application/models/ask_hr.php
+++ staff/application/models/ask_hr.php
@@ -29,6 +29,18 @@ class Ask_hr extends CI_Model {
 			return $num;
 		}
      } //end of askhr function
+
+     function hrhelpdesk($table1, $table2, $condition1, $condition2, $field){
+     	$this->db->select($field);    
+		$this->db->from($table);
+		$this->db->join($table2, $table.'.'.$condition1 .'='. $table2 .'.'. $condition2);
+		
+		$query = $this->db->get();
+		foreach($query->result() AS $q):
+			$arr[] = $q;
+		endforeach;
+		return $arr;
+     }
    
 } // end of class
 
