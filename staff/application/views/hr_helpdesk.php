0a6baf07ca557f1d595a50754d48e99a7ee02f72
Shem Chavez <shem.chavez@tatepublishing.net>
2016-04-13 00:07:54 -0500

    hr helpdesk view udpate

--- staff/application/views/hr_helpdesk.php
+++ staff/application/views/hr_helpdesk.php
@@ -2,36 +2,48 @@
 	th{
 	 	background-color: #CCCCCC;
 	 }
-
-	 .setting_options{
-	 	padding: 8px 60px 8px 10px;
-	 	background-color: #CCCCCC; 
-	 }
-
-	 .setting_pos{
-	 	padding-right: 10px;
+	 td{
+	 	text-align: center;
 	 }
 
 	 a:link, a:visited, a:hover, a:active{
 	 	color: black;
 	 	text-decoration: none;
+	 	background-color: #CCCCCC; 
+	 	padding: 8px 20px 8px 20px;	 
+	 }
+
+	 .settings-bottom{
+	 	margin-bottom: 5px;
+	 }
+
+	 .options-right{
+	 	float: right; 
+	 	margin-top: 12.5px;
 	 }
 	
 </style>
 
 <h2>HR HelpDesk</h2>
-<hr/>
-
 <ul class="tabs">
 	<li class="tab-link current" data-tab="tab-1">New</li>
 	<li class="tab-link" data-tab="tab-2">Active</li>
 	<li class="tab-link" data-tab="tab-3">Resolved</li>
 	<li class="tab-link" data-tab="tab-4">Cancelled</li>
+
+	<div class="options-right">
+		<a href="#">Generate Reports</a>	
+		<a href="#">HR Customer CSatResults</a>
+	</div>
+	
 </ul>
 
+
+<hr/>
 <div id="tab-1" class="tab-content current">
 <!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
   <br>
+  <!--
   <table class="tableInfo fs11px">
       	<tr>
       		<th>Incident #</th>
@@ -41,7 +53,7 @@
       		<th>Priority</th>
       	</tr>
 		<tr>
-      		<td>1</td>
+      		<td>1 <div>Insert your code here!</div></td>
       		<td>Shem</td>
       		<td>04/12/16</td>
       		<td>Char lang</td>
@@ -56,22 +68,43 @@
 	      	</tr>   
       </table>
 
+      -->
+
+      <div id="tbl_570dc0bf52069_wrapper" class="dataTables_wrapper no-footer">
+      	<div class="dataTables_length" id="tbl_570dc0bf52069_length">
+      		<label>
+      		Show
+	      		<select name="tbl_570dc0bf52069_length" aria-controls="tbl_570dc0bf52069" class>
+		      		<option value="10">10</option>
+		      		<option value="25">25</option>
+		      		<option value="50">50</option>
+		      		<option value="100">100</option>
+	      		</select>
+      		entries
+      		</label>
+      	</div>
+      </div>
+
       <br>
       <h2>SETTINGS</h2><br>
-      <table>
-      	<tr>
-      		<td class="setting_pos"><a href="#" class="setting_options">Add Categories</a></td>
-      		<td class="setting_pos"><a href="#" class="setting_options">Edit HR User Permissions</a></td>
-      		<td class="setting_pos"><a href="#" class="setting_options">Edit Message Templates</a></td>
-      		<td class="setting_pos"><a href="#" class="setting_options">Add a Redirection Department</a></td>
-      	</tr>	
-      </table>
-
+	
+	<div class="settings-bottom">
+		<a href="#">Add Categories</a>
+  		<a href="#">Edit HR User Permissions</a>
+  		<a href="#">Edit Message Templates</a>
+  		<a href="#">Add a Redirection Department</a>
+	</div>    	
 </div>
 
 <div id="tab-2" class="tab-content">
 <!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_accounting, 'pending_accounting'); ?> -->
 B
+
+<div class="tiptext">Text
+<div class="description"> Here is the big fat description box</div>
+</div>
+
+
 </div>
 
 <div id="tab-3" class="tab-content">
@@ -82,6 +115,3 @@ C
 <!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
 D
 </div>
-
-
-
