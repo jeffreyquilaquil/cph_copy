4e238b81de6e8fa30eab9412e16998e2b21992b4
Shem Chavez <shem.chavez@tatepublishing.net>
2016-04-12 20:08:52 -0500

    updated view hr_helpdesk

--- staff/application/views/hr_helpdesk.php
+++ staff/application/views/hr_helpdesk.php
@@ -1,55 +1,88 @@
-<?php
-	
-	$currentMedPerson = '';
-	$currentFullFinance = '';
-	$currentAll = '';
-	if( $this->access->accessMedPerson ){
-		$currentMedPerson = 'current';
-	} else if( $this->access->accessFullFinance ){
-		$currentFullFinance = 'current';
-	} else {
-		$currentAll = 'current';
-	}
-	
-?>
+<style type="text/css">
+	th{
+	 	background-color: #CCCCCC;
+	 }
 
+	 .setting_options{
+	 	padding: 8px 60px 8px 10px;
+	 	background-color: #CCCCCC; 
+	 }
+
+	 .setting_pos{
+	 	padding-right: 10px;
+	 }
+
+	 a:link, a:visited, a:hover, a:active{
+	 	color: black;
+	 	text-decoration: none;
+	 }
+	
+</style>
 <h2>HR HelpDesk</h2>
 <hr/>
 
 <ul class="tabs">
-	<li class="tab-link <?php echo $currentMedPerson; ?>" data-tab="tab-1">New <?php echo '('. $cnt_medical .')'; ?></li>
-	<li class="tab-link <?php echo $currentFullFinance; ?>" data-tab="tab-2">Active <?php echo '('. $cnt_accounting .')'; ?></li>
-	<li class="tab-link <?php echo $currentAll; ?>" data-tab="tab-3">Resolved <?php echo '('. $cnt_all .')'; ?></li>
-	<li class="tab-link <?php echo $currentAll; ?>" data-tab="tab-4">Cancelled <?php echo '('. $cnt_all .')'; ?></li>
+	<li class="tab-link" data-tab="tab-1">New</li>
+	<li class="tab-link" data-tab="tab-2">Active</li>
+	<li class="tab-link" data-tab="tab-3">Resolved</li>
+	<li class="tab-link" data-tab="tab-4">Cancelled</li>
 </ul>
 
-<div id="tab-1" class="tab-content <?php echo $currentMedPerson; ?>"><br/>
+<div id="tab-1" class="tab-content"><br/>
+<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
 
-<?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?>
+  <table class="table" style="margin-top: 20px">
+      	<tr class="tr tr-bordered">
+      		<th>Incident #</th>
+      		<th>Customer</th>
+      		<th>Date Submitted</th>
+      		<th>Subject</th>
+      		<th>Priority</th>
+      	</tr>
+		<tr>
+      		<td>1</td>
+      		<td>Shem</td>
+      		<td>04/12/16</td>
+      		<td>Char lang</td>
+      		<td>Needs Attention</td>
+      	</tr>
+	      	<tr>
+	      		<td>2</td>
+	      		<td>Chavez</td>
+	      		<td>04/12/16</td>
+	      		<td>Char lang</td>
+	      		<td>Needs Attention</td>
+	      	</tr>   
+      </table>
+
+      <h2>SETTINGS</h2><br>
+      <table>
+      	<tr>
+      		<td class="setting_pos"><a href="#" class="setting_options">Add Categories</a></td>
+      		<td class="setting_pos"><a href="#" class="setting_options">Edit HR User Permissions</a></td>
+      		<td class="setting_pos"><a href="#" class="setting_options">Edit Message Templates</a></td>
+      		<td class="setting_pos"><a href="#" class="setting_options">Add a Redirection Department</a></td>
+      	</tr>	
+      </table>
+
 </div>
 
-<div id="tab-2" class="tab-content <?php echo $currentFullFinance; ?>"><br/>
-<h3>Pending Approval from Accounting <?php echo '('. $cnt_accounting .')'; ?></h3>
-<?php echo $this->textM->reimbursementTableDisplay($data_query_accounting, 'pending_accounting'); ?>
+<div id="tab-2" class="tab-content"><br/>
+<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_accounting, 'pending_accounting'); ?> -->
+
+
 </div>
 
-<div id="tab-3" class="tab-content <?php echo $currentAll; ?>"><br/>
+<div id="tab-3" class="tab-content"><br/>
 
 	
 </div>
 
-<div id="tab-4" class="tab-content <?php echo $currentMedPerson; ?>"><br/>
-<h3>Pending Approval from Medical Personnel <?php echo '('. $cnt_medical .')'; ?></h3>
-<?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?>
+<div id="tab-4" class="tab-content"><br/>
+<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
+
+
 </div>
 
-
-<script type="text/javascript">
-
-	function showTbl(tbl, p){
-		$('#tbl'+tbl).toggleClass('hidden');
-		$(p).find('a').text($(p).find('a').text() == '[show]' ? '[hide]' : '[show]'); 
-	}
-</script>
 
 
