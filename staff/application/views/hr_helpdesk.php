a66a21236d17e48a90e673381a5e397c0a7f2e65
Shem Chavez <shem.chavez@tatepublishing.net>
2016-04-13 18:58:56 -0500

    datatable new v2

--- staff/application/views/hr_helpdesk.php
+++ staff/application/views/hr_helpdesk.php
@@ -41,84 +41,7 @@
 
 <hr/>
 <div id="tab-1" class="tab-content current">
-<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
   <br>
-  <!--
-  <table class="tableInfo fs11px">
-      	<tr>
-      		<th>Incident #</th>
-      		<th>Customer</th>
-      		<th>Date Submitted</th>
-      		<th>Subject</th>
-      		<th>Priority</th>
-      	</tr>
-		<tr>
-      		<td>1 <div>Insert your code here!</div></td>
-      		<td>Shem</td>
-      		<td>04/12/16</td>
-      		<td>Char lang</td>
-      		<td>Needs Attention</td>
-      	</tr>
-	      	<tr>
-	      		<td>2</td>
-	      		<td>Chavez</td>
-	      		<td>04/12/16</td>
-	      		<td>Char lang</td>
-	      		<td>Needs Attention</td>
-	      	</tr>   
-      </table>
-
-      -->
-      <!--
-      <div id="tbl_570dc0bf52069_wrapper" class="dataTables_wrapper no-footer">
-      	<div class="dataTables_length" id="tbl_570dc0bf52069_length">
-      		<label>
-      		Show
-	      		<select name="tbl_570dc0bf52069_length" aria-controls="tbl_570dc0bf52069" class>
-		      		<option value="10">10</option>
-		      		<option value="25">25</option>
-		      		<option value="50">50</option>
-		      		<option value="100">100</option>
-	      		</select>
-      		entries
-      		</label>
-      	</div>
-      	<div id="tbl_570dc0bf52069_filter" class="dataTables_filter">
-      		<label>
-      			Search
-      			<input type="search" class placeholder aria-controls="tbl_570dd53cac70d">
-      		</label>
-      	</div>
-
-      	<table class="tableInfo fs11px dataTable no-footer" id="tbl_570dd53cac70d" role="grid" aria-describedby="tbl_570dd53cac70d_info">
-      		<thead>
-      			<tr role="row">
-      				<th class="sorting_asc" tabindex="0" aria-controls="tbl_570dd770149b1" rowspan="1" colspan="1" aria-label="Incident # activate to sort column ascending" style="width: 164px;" aria-sort="descending">Incident #</th>
-      				<th class="sorting" tabindex="0" aria-controls="tbl_570dd770149b1" rowspan="1" colspan="1" aria-label="Customer activate to sort column ascending" style="width: 173px;">Customer</th>
-      				<th class="sorting" tabindex="0" aria-controls="tbl_570dd770149b1" rowspan="1" colspan="1" aria-label="Date Submitted activate to sort column ascending" style="width: 169px;">Date Submitted</th>
-      				<th class="sorting" tabindex="0" aria-controls="tbl_570dd770149b1" rowspan="1" colspan="1" aria-label="Subject activate to sort column ascending" style="width: 188px;">Subject</th>
-      				<th class="sorting" tabindex="0" aria-controls="tbl_570dd770149b1" rowspan="1" colspan="1" aria-label="Priority activate to sort column ascending" style="width: 79px;">Priority</th>
-      			</tr>
-      		</thead>
-      		<tbody>
-      			<tr class="odd">
-      				<td>1</td>
-      				<td>Shem</td>
-      				<td>04/14/16</td>
-      				<td>A</td>
-      				<td>B</td>
-      			</tr>
-      		</tbody>
-      	</table>
-      <div class="dataTables_info" id="tbl_570dd9e4944c7_info" role="status" aria-live="polite">Showing 0 to 0 of 0 entries</div>
-      <div class="dataTables_paginate paging_simple_numbers" id="tbl_570dd9e4944c7_paginate">
-	      	<a class="paginate_button previous disabled" aria-controls="tbl_570dd9e4944c7" data-dt-idx="0" tabindex="0" id="tbl_570dd9e4944c7_previous">Previous</a><span></span>
-	      	<a class="paginate_button next disabled" aria-controls="tbl_570dd9e4944c7" data-dt-idx="1" tabindex="0" id="tbl_570dd9e4944c7_next">Next</a>
-      </div>
-      
-      </div>
-
-      -->
 
       <table id="dt_new">
       	<thead>
@@ -160,7 +83,7 @@
 </div>
 
 <div id="tab-2" class="tab-content">
-<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_accounting, 'pending_accounting'); ?> -->
+
 B
 
 <div class="tiptext">Text
@@ -175,6 +98,6 @@ C
 </div>
 
 <div id="tab-4" class="tab-content">
-<!-- <?php echo $this->textM->reimbursementTableDisplay($data_query_medical, 'pending_med'); ?> -->
+
 D
 </div>
