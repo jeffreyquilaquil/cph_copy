556a79c00127ac3de84d1cf671fc2880c8e152d1
Shem Chavez <shem.chavez@tatepublishing.net>
2016-04-13 22:41:42 -0500

    updated helpdesk and incident info ui

--- staff/application/views/hr_helpdesk.php
+++ staff/application/views/hr_helpdesk.php
@@ -6,6 +6,10 @@
 	 	text-align: center;
 	 }
 
+	 a{
+		text-decoration: none;
+	} 
+
 	 a.other_links:link, a.other_links:visited, a.other_links:hover, a.other_links:active{
 	 	color: black;
 	 	text-decoration: none;
@@ -24,39 +28,28 @@
 
 	.td_hover {
 	    position: relative;
-	    display: inline-block;
-	    text-align: center;
+	    display: block;
+	    
 	}
 
-.td_hover .show_details {
-    visibility: hidden;
-    width: 120px;
-    background-color: black;
-    color: #fff;
-    text-align: center;
-    border-radius: 6px;
-    padding: 5px 0;
-    position: absolute;
-    z-index: 1;
-    top: -5px;
-    left: 110%;
-}
-
-.td_hover .show_details::after {
-    content: "";
-    position: absolute;
-    top: 50%;
-    right: 100%;
-    margin-top: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: transparent black transparent transparent;
-}
-.td_hover:hover .show_details {
-    visibility: visible;
-} 
+	.td_hover .show_details {
+	    visibility: hidden;
+	    width: 200px;
+	    background-color: black;
+	    color: #fff;
+	    text-align: center;
+	    border-radius: 6px;
+	    padding: 20px;
+	    position: absolute;
+	    z-index: 1;
+	    top: -5px;
+	    left: 110%;
+	}
 
 
+	.td_hover:hover .show_details {
+	    visibility: visible;
+	}
 
 </style>
 
--- staff/application/views/hr_incidentinfo.php
+++ staff/application/views/hr_incidentinfo.php
@@ -1,21 +1,21 @@
 <style type="text/css">
 	.note{
 		font-style: italic;
+		font-size: x-small;
 	}
 </style>
 
 <div>
-	<table>
+	<table class="tableInfo">
 		<tr>
-			<td colspan="2"><h2>HR Incident Number 000003</h2></td>
+			<td colspan="2">
+				<h2>HR Incident Number 000003<br><small>You have owned responsibility for incident number 000003</small></h2>
+
+			</td>
 			<td></td>
 		</tr>
 		<tr>
-			<td colspan="2"><h3>You have owned responsibility for incident number 000003</h3></td>
-			<td></td>
-		</tr>
-		<tr>
-			<td>Customer</td>
+			<td >Customer</td>
 			<td>Ann Margaret Yap</td>
 		</tr>
 		<tr>
@@ -35,21 +35,17 @@
 			<td>
 				<select required>
 				<option value="">Compensation and Benefits</option>
-				<option value="">Compensation</option>
-				<option value="">Benefits-SSS</option>
-				<option value="">Benefits-Philhealth</option>
-				<option value="">Benefits-Pag-IBIG</option>
-				<option value="">Benefits-Leave Application</option>
-				<option value="">Benefits-Medical Reimbursement</option>
-				<option value="">Benefits-Offset Application</option>
-				<option value="">Code of Conduct Policy</option>
-				<option value="">Disciplinary</option>
-				<option value="">Health Insurance</option>
-				<option value="">Payroll Related</option>
-				<option value="">CareerPH/Staff</option>
-				<option value="">Organizational Chart</option>
+				<option></option>
 			</select>
-			<a href="#">Assign Category</a>
+			<br>
+			<span id="add_category" style="text-decoration: underline;cursor: pointer;">Add Category</span>
+			
+			<div id="show_add_category">
+			<br>
+				<form>
+					Category name: <input type="text" name="category_name"> <input type="submit" value="Add">
+				</form>	
+			</div>
 			</td>
 		</tr>
 		<tr>
@@ -66,7 +62,7 @@
 			</td>
 		</tr>
 		<tr>
-			<td>Details of the incident</td>
+			<td valign="top">Details of the incident</td>
 			<td>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -74,39 +70,44 @@
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.			
-			<br>
+			<br><br>
 			<span class="note">
 				Instruction to HR: Search <a href="#">www.employee.tatepublishing.net</a> first to see if the
 				answer to the employee's inquiry above can be found there. If it is, then click on the appropriate
 				<b>RESOLVE</b> action below.
 			</span>
+			<br><br>
+			<div style="background: #CCCCCC; font-weight: bold; width: 100%; padding: 5px 0px 5px 5px">Resolution Options</div>
 			<br>
-			<span style="background: #CCCCCC; font-weight: bold; width: 100%">Resolution Options</span>
-			<a href="#">The answer can be found in employee.tatepublishing.net</a><br>
-			<a href="#">Send custom resolution response</a><br>
-			<a href="#">This is not an HR inquiry, Redirect to another department</a><br>
+			<a href="#">The answer can be found in employee.tatepublishing.net</a><br><br>
+			<a href="#">Send custom resolution response</a><br><br>
+			<a href="#">This is not an HR inquiry, Redirect to another department</a><br><br>
 			<!-- APPEAR ONLY IF HR STAFF SELECT YES TO INVESTIGATION REQUIRED ITEM REMOVE -->
 			<div id="appear_furtherinfo">
-				<a href="#">Further information (investigation) is required</a> 
+				<a href="#">Further information (investigation) is required</a><br>
 			</div>
 			</td>
 		</tr>
 	</table>
-
-	<table>
+	<table class="tableInfo">
 		<tr>
-			<th>Notes</th>
-			<th colspan="2"><a href="#">Add Note</a></th>
-			<th></th>
+			<th align="left">Notes</th>
+			<th colspan="2" align="right"><a href="#">Add Note</a></th>
+			
 		</tr>
 		<tr>
+			<td></td>
+			<td></td>
 			<td></td>
 		</tr>
 	</table>
 </div>
 
 <script type="text/javascript">
-	$(document).ready(function(){
+	$(function(){
+
+		$("#appear_furtherinfo").hide();
+		
 		$("#yes").click(function(){
 	    	$("#appear_furtherinfo").show();
 		});
@@ -115,4 +116,14 @@
 		    $("#appear_furtherinfo").hide();
 		});
 	});	
+
+	$(function(){
+
+		$("#show_add_category").hide();
+		
+		$("#add_category").click(function(){
+	    	$("#show_add_category").show();
+		});
+	
+	});
 </script>
