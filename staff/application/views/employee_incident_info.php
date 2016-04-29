d0f16e88aa60930cc5980a84202f4dc6b7a9446d
Shem Chavez <shem.chavez@tatepublishing.net>
2016-04-28 21:12:28 -0500

    employee dashboard

---
+++ staff/application/views/employee_incident_info.php
@@ -0,0 +1,120 @@
+<style type="text/css">
+	 .datatable td{
+	 	text-align: center;
+	 }
+	 
+	 a{
+	 	text-decoration: underline;
+	 }
+
+	 input[type=text]{
+
+		width: 100%;
+	}
+
+</style>
+<div id="employee_incident_info_form">
+	<h2>HR HelpDesk</h2>
+	<ul class="tabs">
+		<li class="dbold tab-link current" id="new_tab" data-tab="tab-1">My Incidents</li>
+	</ul>
+	<hr>
+	<div id="tab-1" class="tab-content current">
+		<table class="datatable">
+	  	<thead>
+		  	<tr>
+			  	<th>Incident #</th>
+		  		<th>Date Submitted</th>
+		  		<th>Subject</th>
+		  		<th>Send Message to HR</th>
+		  		<th>Cancel Incident</th>	
+		  	</tr>
+	  	</thead>
+	  		<tr>
+	      		<td>incident number</td>
+	      		<td>date submitted</td>
+	      		<td>subject</td>
+	      		<td><a id="send_message_to_hr">Send Message to HR</a></td>
+	      		<td><a id="cancel_incident">Cancel Incident</a></td>
+	  		</tr>	     
+	</table>
+	</div>
+</div>
+
+<div id="send_message_to_hr_form">
+	<table class="tableInfo">
+	<tr>
+		<td colspan="2"><h2>Send Email</h2></td>
+	</tr>
+	<tr>
+		<td>From:</td>
+		<td>sample</td>
+	</tr>
+	<tr>
+		<td>To:</td>
+		<td>hr.cebu@tatepublishing.net</td>
+	</tr>
+	<tr>
+	<td>Subject:</td>
+	<td><input type="text" name="" value="" placeholder=""></td>
+	</tr>
+	<tr>
+		<td colspan="2"><textarea class="hidden tiny" style="height:200px; resize: none; font-size: left;"></textarea></td>
+	</tr>
+	<tr>
+		<td align="left"><a id="back_to_home">Back</a></td>
+		<td align="right"><input type="submit" name="" value="Send" class="btngreen"></td>
+	</tr>
+	</table>
+</div>
+	
+<div id="cancel_incident_form"> 
+	<table class="tableInfo">
+		<tr>
+			<td colspan="2"><h2>You are about tp cancel HR incident number 000003<br><small>Please tell us why (optional):</small></h2></td>			
+		</tr>
+		<tr>
+			<td colspan="2"><textarea style="height:200px; resize: none; font-size: left;"></textarea></td>
+		</tr>
+		<tr>
+			<td align="left"><a id="back_to_home">Back</a></td>
+			<td align="right"><input type="button" name="" class="btngreen" value="Cancel Incident"></td>
+		</tr>	
+	</table>
+</div>
+
+<script type="text/javascript" src="<?= $this->config->base_url() ?>js/tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+
+	$(function(){
+				
+		$('#cancel_incident_form').hide();
+		$('#cancel_incident').click(function() {
+			
+			$('#employee_incident_info_form').hide();
+			$('#cancel_incident_form').show();
+		});
+
+		$('#send_message_to_hr_form').hide();
+		$('#send_message_to_hr').click(function() {
+			
+			$('#employee_incident_info_form').hide();
+			$('#send_message_to_hr_form').show();
+		});
+
+		$('a#back_to_home').click(function() {
+			
+			$('#send_message_to_hr_form').hide();
+			$('#cancel_incident_form').hide();
+			$('#employee_incident_info_form').show();
+		});
+
+		// ===== DISPLAY TOOLBAR IN TEXTAREA =====
+		tinymce.init({
+		selector: "textarea.tiny",	
+		menubar : false,
+		toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link table code image"
+		});	
+	});
+
+</script>
--- staff/application/views/includes/leftcontent.php
+++ staff/application/views/includes/leftcontent.php
@@ -80,13 +80,14 @@ if($this->user!=false && $this->uri->segment(1)=='schedules'){
 			echo '<li><a href="'.$this->config->base_url().'fileleave/" class="iframe">File for a Leave/Offset</a></li>';
 			
 			if(isset($row->empStatus) && $row->empStatus=='probationary')
-				echo '<li><a href="'.$this->config->base_url().'evaluationself/" class="iframe">Submit Self-Evaluation</a></li>';
-			
+			echo '<li><a href="'.$this->config->base_url().'evaluationself/" class="iframe">Submit Self-Evaluation</a></li>';
 			echo '<li><a href="'.$this->config->base_url().'changepassword/" class="iframe">Update My Password</a></li>';
 			echo '<li><a href="'.$this->config->base_url().'upsignature/" class="iframe">Update My Signature</a></li>';
 			echo '<li><a href="'.$this->config->base_url().'requestcoe/" class="iframe">Request for Certificate of Employment</a></li>';
 			echo '<li><a href="'.$this->config->base_url().'medrequest/" class="iframe">Request for Medicine Reimbursement</a></li>';
-		}
+			echo '<li><a href="'.$this->config->base_url().'hr_cs/" class="iframe">Ask an HR Question</a></li>';
+			echo '<li><a href="'.$this->config->base_url().'hr_cs/test" class="iframe">Employee Dashboard</a></li>';
+	}
 		if(($content=='staffinfo' || $this->uri->segment(1)=='timecard') && $this->user->username != $row->username && ($this->access->accessFullHR==true || $this->commonM->checkStaffUnderMe($row->username))){
 			echo '<li><a href="'.$this->config->base_url().'staffinfo/'.$row->username.'/" '.(($content=='staffinfo')?'class="current"':'').'>'.trim($row->fname).'\'s Info</a></li>';
 			echo '<li><a href="'.$this->config->base_url().'timecard/'.$row->empID.'/calendar/" '.(($this->uri->segment(1)=='timecard')?'class="current"':'').'>Timecard and Payroll</a></li>';
@@ -102,14 +103,14 @@ if($this->user!=false && $this->uri->segment(1)=='schedules'){
 		if((($content!='staffinfo' && $this->uri->segment(1)!='timecard') || ($content=='staffinfo' && $current=='myinfo')) && ($this->access->accessFullHR==true || $this->user->level>0)){
 			echo '<li><a href="'.$this->config->base_url().'generatecode/" class="iframe">Generate Code</a></li>';
 			echo '<li><a href="'.$this->config->item('career_url').'/jobrequisition.php" target="_blank">Request for Job Requisition</a></li>';		
+			
 		}
 				
 		if($this->access->accessFull==true)
 			echo '<li><a href="'.$this->config->base_url().'adminsettings/'.$row->empID.'/" class="iframe">Other Settings</a></li>';
-	
-		echo '<li><a href="'.$this->config->base_url().'reportviolation/" class="iframe">Report a COC Violation</a></li>';	
-		echo '<li><a href="'.$this->config->base_url().'referafriend/" class="iframe" style="padding:15px 10px;"><b>Refer a Friend to Work in Tate</b></a></li>';
-		echo '<li><a href="'.$this->config->base_url().'hr_cs/" class="iframe">Ask an HR Question</a></li>';
+			echo '<li><a href="'.$this->config->base_url().'reportviolation/" class="iframe">Report a COC Violation</a></li>';		
+			echo '<li><a href="'.$this->config->base_url().'referafriend/" class="iframe" style="padding:15px 10px;"><b>Refer a Friend to Work in Tate</b></a></li>';
+
 	echo '</ul>';	
 	echo '</center>';
 	
