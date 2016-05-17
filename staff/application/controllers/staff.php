c07d6e3302487d4facea6b76110431e6525a5091
marjune <marjune.abellana@tatepublishing.net>
2016-05-16 19:06:40 -0500

    disapprove cis

--- staff/application/controllers/staff.php
+++ staff/application/controllers/staff.php
@@ -2416,7 +2416,7 @@ class Staff extends MY_Controller {
 				
 				$insCIS = array(
 							'empID_fk' => $id,
-							'datefiled' => 'NOW()',
+							'datefiled' => date('Y-m-d H:i:s'),
 							'effectivedate' => date('Y-m-d', strtotime($_POST['effectiveDate'])),
 							'changes' => json_encode($updatetext),
 							'dbchanges' => json_encode($updateArr),
@@ -2581,6 +2581,7 @@ class Staff extends MY_Controller {
 				$data['pending'] = $this->dbmodel->getQueryResults('staffCIS', 'staffCIS.*, CONCAT(fname," ",lname) AS name, username, (SELECT CONCAT(fname," ",lname) AS n FROM staffs s WHERE s.empID=staffs.supervisor) AS supName, (SELECT CONCAT(fname," ",lname) AS n FROM staffs WHERE empID=preparedby) AS prepby, updatedby', 'status=0', 'LEFT JOIN staffs ON empID=empID_fk');
 				$data['approved'] = $this->dbmodel->getQueryResults('staffCIS', 'staffCIS.*, CONCAT(fname," ",lname) AS name, username, (SELECT CONCAT(fname," ",lname) AS n FROM staffs s WHERE s.empID=staffs.supervisor) AS supName, (SELECT CONCAT(fname," ",lname) AS n FROM staffs WHERE empID=preparedby) AS prepby, updatedby', 'status=1', 'LEFT JOIN staffs ON empID=empID_fk');
 				$data['done'] = $this->dbmodel->getQueryResults('staffCIS', 'staffCIS.*, CONCAT(fname," ",lname) AS name, username, (SELECT CONCAT(fname," ",lname) AS n FROM staffs s WHERE s.empID=staffs.supervisor) AS supName, (SELECT CONCAT(fname," ",lname) AS n FROM staffs WHERE empID=preparedby) AS prepby, updatedby', 'status=3', 'LEFT JOIN staffs ON empID=empID_fk');
+				$data['disapproved'] = $this->dbmodel->getQueryResults('staffCIS', 'staffCIS.*, CONCAT(fname," ",lname) AS name, username, (SELECT CONCAT(fname," ",lname) AS n FROM staffs s WHERE s.empID=staffs.supervisor) AS supName, (SELECT CONCAT(fname," ",lname) AS n FROM staffs WHERE empID=preparedby) AS prepby, updatedby', 'status=2', 'LEFT JOIN staffs ON empID=empID_fk');
 			}
 		}
 		
--- staff/application/views/staffcis.php
+++ staff/application/views/staffcis.php
@@ -4,6 +4,7 @@
 	<li class="tab-link current" data-tab="tab-1">Newly Requested CIS (<?= count($pending) ?>)</li>
 	<li class="tab-link" data-tab="tab-2">Approved CIS (<?= count($approved) ?>)</li>
 	<li class="tab-link" data-tab="tab-3">Done (<?= count($done) ?>)</li>
+	<li class="tab-link" data-tab="tab-4">Disapproved (<?= count($disapproved) ?>)</li>
 </ul>
 <div id="tab-1" class="tab-content current">	
 	<br/>
@@ -23,5 +24,11 @@
 	<br/>
 	<?php
 		echo $this->textM->displaycis($done, 3);
+	?>
+</div>
+<div id="tab-4" class="tab-content">
+	<br/>
+	<?php
+		echo $this->textM->displaycis($disapproved, 2);
 	?>
 </div>
