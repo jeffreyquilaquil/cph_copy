919b1f5c0769c165b1d1ded16c484a11a2c2078b
marjune <marjune.abellana@tatepublishing.net>
2016-07-11 18:39:04 -0500

    temp id

--- create_temp_id.php
+++ create_temp_id.php
@@ -1,32 +1,33 @@
 <?php 
-ini_set('display_errors', 1);
-$_POST = array(
-	'startdate' => 'July 08, 2016',
-	'bdate' => 'Janury 01, 2000',
-	'sss' => '111111111',
-	'philhealth' => '11111111111111',
-	'hdmf' => '111111111111111',
-	'tin' => '1111111111111111111',
-	'e_contact_person' => 'Contact Person',
-	'e_contact_address' => 'Contact Address',
-	'e_contact_number' => 'Contact Number',
-	'e_contact_relationship' => 'Relationship',
-	'fname' => 'Marjune',
-	'lname' => 'Abellana',
-	'username' => 'jmonares'
-	);
+
+$username = $_GET['username'];
+if( !isset($username) AND empty($username) ){
+	echo 'Please provide the username.';
+	exit();
+}
+
+require 'config.php';
+
+if(!isset($_SESSION['u']) || !in_array($_SESSION['u'], $authorized)){
+	header("Location: login.php");
+	exit();
+}
+
+$hire = $db->selectSingleQueryArray('staffs', '*' , 'username = "'.$username.'"');
 
 
-$tmp_id_filename = 'tmp_id_'.$_POST['username'];
-$full_path = '/home/careerph/public_html/staff/uploads/staffs/'. $_POST['username'];
 
-$signature_file =  $full_path.'/signature.JPG';
-	$tmp_id_file = $full_path.'/tmp_id_jmonares.JPG';
+
+$tmp_id_filename = 'tmp_id_'.$username;
+$full_path = '/home/careerph/public_html/staff/uploads/staffs/'. $username;
+
+$signature_file =  $full_path.'/signature.png';
+	$tmp_id_file = $full_path.'/tmp_id_'.$username.'.JPG';
 $d = time();
-$save_path_sig = $full_path.'/signature_resized'.$d.'.jpg';
+$save_path_sig = $full_path.'/signature_resized'.$d.'.png';
 $save_path_id = $full_path.'/tmp_id_resized'.$d.'.jpg';
 	
-$signature_file = resize_image( $signature_file, 100, 70, $save_path_sig, 'jpg');
+$signature_file = resize_image( $signature_file, 100, 70, $save_path_sig, 'png');
 $tmp_id_file = resize_image( $tmp_id_file, 105, 150, $save_path_id, 'jpg' );
 
 
@@ -42,39 +43,39 @@ $tmp_id_file = resize_image( $tmp_id_file, 105, 150, $save_path_id, 'jpg' );
 			$pdf->setTextColor(0, 0, 0);
 			
 			$pdf->setXY(42, 34);
-			$pdf->Write(0, $_POST['startdate']);
+			$pdf->Write(0, $hire['startDate']);
 			//hbd
 			$pdf->setXY(41, 38.5);		
-			$pdf->Write(0, $_POST['bdate'] );
+			$pdf->Write(0, $hire['bdate'] );
 			
 			$pdf->setXY(41, 42.5);		
-			$pdf->Write(0, $_POST['sss'] );
+			$pdf->Write(0, $hire['sss'] );
 			
 			$pdf->setXY(41, 47);		
-			$pdf->Write(0, $_POST['philhealth'] );
+			$pdf->Write(0, $hire['philhealth'] );
 			
 			$pdf->setXY(42, 51);		
-			$pdf->Write(0, $_POST['hdmf'] );
+			$pdf->Write(0, $hire['hdmf'] );
 			
 			$pdf->setXY(40, 55.5);		
-			$pdf->Write(0, $_POST['tin'] );
+			$pdf->Write(0, $hire['tin'] );
 			
 			$pdf->setXY(80, 34);		
-			$pdf->Write(0, $_POST['e_contact_person'] );
+			$pdf->Write(0, $hire['e_contact_person'] );
 			
 			$pdf->setXY(82, 38.5);		
-			$pdf->Write(0, $_POST['e_contact_address'] );
+			$pdf->Write(0, $hire['e_contact_address'] );
 			
 			$pdf->setXY(84, 42.5);		
-			$pdf->Write(0, $_POST['e_contact_number'] );
+			$pdf->Write(0, $hire['e_contact_number'] );
 			
 			$pdf->setXY(85, 47);		
-			$pdf->Write(0, $_POST['e_contact_relationship'] );
+			$pdf->Write(0, $hire['e_contact_relationship'] );
 			
 			$pdf->SetFont('Arial','B',9);
 			//$pdf->setTextColor(255, 255, 255);
 			$pdf->setTextColor(0, 0, 0);	
-		    $full_name = $_POST['fname'].' '.$_POST['lname'];	
+		    $full_name = $hire['fname'].' '.$hire['lname'];	
 			$full_name = strtoupper( $full_name );
 			$pdf->setXY(67, 118);		
 			$pdf->Write(0, $full_name );
