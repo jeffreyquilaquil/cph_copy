e66f58db23606faa47283e32055c310b96bcc9e5
Fitt Brandon Lariosa <fitt.lariosa@tatepublishing.net>
2016-09-02 00:44:47 -0500

    enye

--- staff/application/models/staffmodel.php
+++ staff/application/models/staffmodel.php
@@ -476,7 +476,7 @@ class Staffmodel extends CI_Model {
 		
 		$pdf->SetFont('Arial','B',14);
 		$pdf->setXY(85, 92);
-		$pdf->Write(0, $row->name);
+		$pdf->Write(0, utf8_decode($row->name));
 		$pdf->setXY(85, 101);
 		$pdf->Write(0, date('F d, Y',strtotime($row->startDate)));
 		$pdf->setXY(85, 110);
