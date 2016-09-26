0be4c61e2d06be63caaad3e2fe1d6d6ebf61808a
Jeffrey Quilaquil <jeffrey.quilaquil.tatepublishing.net>
2016-09-26 02:53:01 -0500

    behavioral and technical evaluation questions

--- staff/application/views/evaluations/questionnaires.php
+++ staff/application/views/evaluations/questionnaires.php
@@ -1,10 +1,8 @@
-<div style="margin-bottom:15px;">
-	<div class="pane" style="width:300px;">
-		YOu are here
+<div style="margin-bottom:15px;text-align:center">
+	<div class="pane" style="width:100%;">
+		<a href="questionnaires/behavioralQuestions"><button class="btnclass">Behavioral</button></a>
 	</div>
-	<div class="pane">
-I was here
-
+	<div class="pane" style="width:100%">
+		<a href="questionnaires/technicalQuestions/0"><button class="btnclass">Technical</button></a>
 	</div>
 </div>
-
