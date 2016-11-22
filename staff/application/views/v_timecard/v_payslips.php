ddb98feb1c9b855198853a34c87a7edfc253d3ec
marjune <marjune.abellana@tatepublishing.net>
2016-11-21 21:47:58 -0600

    13th month view

--- staff/application/views/v_timecard/v_payslips.php
+++ staff/application/views/v_timecard/v_payslips.php
@@ -13,4 +13,17 @@ if(count($dataPayslips)==0){
 			echo '</tr>';   
 		}
 	echo '</table>';
-} ?>
+}
+
+if( count($data_13thMonth) > 0 ){
+	echo '<table class="tableInfo">';
+		foreach( $data_13thMonth as $data ){
+			echo '<tr>';
+				echo '<td>13th Month '.date('F d', strtotime($data->periodFrom)).' - '.date('F d, Y', strtotime($data->periodTo)).'</td>';
+				echo '<td></td>';
+				echo '<td width="50px"><a href="'.$this->config->base_url().'timecard/detail13thmonth/'.$data->tcmonthID.'/"><img src="'.$this->config->base_url().'css/images/btn-view-details.png"/></a></td>';
+				echo '<td width="50px"><a href="'.$this->config->base_url().'timecard/detail13thmonth/'.$data->tcmonthID.'/?show=pdf" target="_blank"><img src="'.$this->config->base_url().'css/images/pdf-icon.png" width="25px"/></a></td>';
+			echo '</tr>';
+		}
+	echo '</table>';
+}
