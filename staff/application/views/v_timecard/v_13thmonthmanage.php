40490496aee612d3de228c3c7dc688a01e178669
Fitt Brandon Lariosa <fitt.lariosa@tatepublishing.net>
2016-12-05 17:57:19 -0600

    13th month

--- staff/application/views/v_timecard/v_13thmonthmanage.php
+++ staff/application/views/v_timecard/v_13thmonthmanage.php
@@ -10,6 +10,62 @@
 <h2>Generated 13th Month</h2>
 
 <hr/>
+
+Select Year: 
+<select name='yearchange' style="padding:2px">
+<?php
+	$year = isset($_GET['year'])? $_GET['year']:date('Y');
+
+	for($i = 2016; $i < 2020; $i++){
+		$selected = '';
+		if($year == $i){
+			$selected = 'selected';
+		}
+		echo "<option $selected value='$i'>$i</option>";
+	}
+?>
+</select>
+
+<?php
+	$totalBasic = 0;
+	$totaladjustment = 0;
+	$total13thmonth = 0;
+	$totalPublished = 0;
+
+	foreach($queryData AS $data){
+		$totalBasic += $data->totalBasic;
+		$totaladjustment += $data->totalDeduction;
+		$total13thmonth += $data->totalAmount;
+		if($data->tc13thMonthPublished > 0)
+			$totalPublished += $data->totalAmount;
+	}
+?>
+
+<div style="width:70%">
+	<table class="tableInfo tacenter">
+		<tr class="trhead">
+			<td>Year</td>
+			<td>Number Generated</td>
+			<td>Total Basic Pay</td>
+		</tr>
+		<tr>
+			<td><?=$year?></td>
+			<td><?=count($queryData)?></td>
+			<td>Php <?=$this->payrollM->formatNum($totalBasic)?></td>
+		</tr>
+		<tr class="trhead">
+			<td>Total Adjustments</td>
+			<td>Total 13th Month</td>
+			<td>Total Published 13th Month</td>
+		</tr>
+		<tr>
+			<td>Php <?=$this->payrollM->formatNum($totaladjustment)?></td>
+			<td style="border:2px solid red;"><b>Php <?=$this->payrollM->formatNum($total13thmonth)?></b></td>
+			<td style="border:2px solid red;"><b>Php <?=$this->payrollM->formatNum($totalPublished)?></b></td>
+		</tr>
+	</table>
+</div>
+
 <div class='selectionDiv' style="padding-top:5px;">
 	<a class="cpointer" id="selectAll">Select All</a> | <a class="cpointer" id="deselectAll">Deselect All</a>
 </div>
@@ -31,11 +87,17 @@
 	<tbody>
 <?php
 	foreach($queryData AS $data){
-		echo '<tr>';
+		$style = '';
+		if( $data->tc13thMonthPublished > 0 )
+			$style = 'style= "background-color:#ccffcc"';
+
+		echo '<tr '.$style.'>';
 			echo '<td><input type="checkbox" class="classCheckMe" name="id_[]" data-empid="'.$data->empID_fk.'" value="'.$data->tcmonthID.'" /></td>';
 			echo '<td>'.$data->lname.', '.$data->fname.'</td>';
 			echo '<td>'.$this->textM->convertNumFormat($data->totalBasic).'</td>';
 			echo '<td>'.$this->textM->convertNumFormat($data->totalDeduction).'</td>';
+			
+
 			echo '<td><b>Php '.$this->textM->convertNumFormat($data->totalAmount).'</b></td>';
 			echo '<td>'.date('M', strtotime($data->periodFrom)).' - '.date('M Y', strtotime($data->periodTo)).'</td>';
 			echo '<td>';
@@ -60,7 +122,7 @@
 		<tr>
 			<td colspan="7">
 		<?php if( $this->access->accessFullFinance == true ){
-			echo '<p>On selected items: <input type="submit" onClick="confirmMsg();" name="delete_13th_record" class="btnclass" value="Delete" /> <input type="button" name="regenerate" class="btnclass" value="Regenerate" /></p>';
+			echo '<p>On selected items: <input type="submit" onClick="confirmMsg();" name="delete_13th_record" class="btnclass" value="Delete" /> <input type="button" name="regenerate" class="btnclass" value="Regenerate" /> <input type="button" name="publish" class="btnclass" value="Publish (all)" /> </p>';
 		}
 		?>
 			</td>
@@ -80,6 +142,31 @@ $(function(){
 		countChecked();
 	});
 
+	$('select[name="yearchange"]').change(function(){
+		var year = $(this).val();
+
+		var myhref = "<?= $this->config->base_url().'timecard/manage13thmonth/?year=' ?>"+year;
+		window.location.href = myhref;
+	});
+
+	$('input[name="publish"]').click(function(){
+		var ids =  checkIfSelected('payID');
+		var payID = 'all';
+		if(ids){
+			payID = ids;
+		}
+
+		var p = confirm('Confirm Publish?');
+		if(p){
+			var year = $('select[name="yearchange"]').val();
+
+			var myhref = "<?= $this->config->base_url().'timecard/manage13thmonth/?year=' ?>"+year+"&payID="+payID+"&method=publish";
+			//alert(myhref);
+			window.location.href = myhref;
+			//alert(payID);
+		}
+	});
+
 	function countChecked(){
 		var countCheck = 0;
 		$('.classCheckMe').each(function(){
@@ -89,6 +176,12 @@ $(function(){
 		});
 		$('.selectionLabel').remove();
 		$('.selectionDiv').append('<div class="selectionLabel"><strong><i>'+countCheck+' Selected<i></strong></div>');
+		if(countCheck > 0){
+			$('input[name="publish"]').val('Publish ('+countCheck+')');
+		}
+		else{
+			$('input[name="publish"]').val('Publish (all)');	
+		}
 	}
 
 	$('#deselectAll').click(function(){
@@ -96,7 +189,7 @@ $(function(){
 		countChecked();
 	});
 	$('input[name="regenerate"]').click(function(){
-		empIDs = checkIfSelected();
+		empIDs = checkIfSelected('empID');
 		if(empIDs==false){
 			alert('Please select employee first.');
 		}else{
@@ -108,20 +201,25 @@ $(function(){
 	$("#dtable13 tfoot tr th select:first").css( "width", "150px" );
 });
 
-function checkIfSelected(){
-	var empIDs = '';
+function checkIfSelected(idToCheck){
+	var id = '';
 	var cars = [];
 	$('.classCheckMe').each(function(){
 		if($(this).is(':checked')){
 			cars.push($(this).val());
-			empIDs = empIDs+$(this).data('empid')+',';
+			if(idToCheck == 'payID'){
+				id = id+$(this).val()+',';
+			}
+			else{
+				id = id+$(this).data('empid')+',';
+			}
 		}
 	});
 	
 	if(cars.length==0){
 		return false;			
 	}else{
-		return empIDs;
+		return id;
 	}
 }
 
